f533d6c373f1545e283fbb7b2600eebe26a21c94
Felipe Oliveira <lipe111094@hotmail.com>
2023-03-28 11:53:13 -0300

    CSS Configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
+import styles from "./App.css";
 import { ListProducts } from "./Pages/listProducts/ListProducts";
 import { ProductsId } from "./Pages/productsId/ProductsId";
 import { Home } from "./Pages/Home";
@@ -7,12 +7,15 @@ import { Home } from "./Pages/Home";
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/products/:id" element={<ProductsId />} />
-        </Routes>
-      </BrowserRouter>
+      <div className={styles.header}>
+        <h1>  </h1>
+      </div>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/products/:id" element={<ProductsId />} />
+          </Routes>
+        </BrowserRouter>
     </>
   );
 }
--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -1,9 +1,15 @@
-import styles from './Footer.module.css';
+import styles from "./Footer.module.css";
 
-export function Footer(){
-    return(
-        <div>
-            <h1>Footer Aqui!</h1>
-        </div>
-    )
+export function Footer() {
+  return (
+    <div className={styles.footer}>
+      <h3 className={styles.description}>
+        Loja Fictícia, desenvolvida por, Felipe Oliveira, para Teste
+        Desenvolvedor Front-End
+      </h3>
+      <h3 className={styles.redes}>Redes e Diretórios</h3>
+      <a><img src=""/> GitHub</a>
+      <a><img/> LinkedIn</a>
+    </div>
+  );
 }
